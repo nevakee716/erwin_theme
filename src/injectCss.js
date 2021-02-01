1c28ec0aca891faaa6385810a0b71a137065c971
nevakee716 <nevakee716@gmail.com>
2021-02-01 22:41:33 +0100

    fix

--- src/injectCss.js
+++ src/injectCss.js
@@ -8,20 +8,27 @@ function addCss(rule) {
 }
 
 // CSS rules
-
+let halfSize = "{ width : calc(50% - 1.5rem);}";
+let fullSize = "{ width : calc(100% - 0.75rem); min-width: 600px;}";
 //list box title
-let rule = "div.cw-property-details-right {width : calc(50% - 1.5rem)}";
-rule += "  div.property-box { width: calc(50% - 0.75rem); min-width: 600px;}";
-rule += '  div.property-box[class*="full"] { width: calc(100% - 0.75rem);}';
-rule += '  ul.cw-accordion-header[class*="half"] { width: calc(50% - 1.5rem);}';
-rule += "  ul.cw-accordion-header { width: calc(100% - 0.75rem);}";
+let rule = "div.cw-property-details-right { width : calc(50% - 1.5rem);}";
+rule += "  div.property-box " + halfSize;
+rule += '  div.property-box[class*="full"] ' + fullSize;
+rule += "  div.cw-accordion-content div.property-box {width: calc(50% - 0.75rem);}";
+rule += '  div.cw-accordion-content div.property-box[class*="full"] {width: 100%;}' ;
 
-rule += '  div.CwPropertiesLayoutHelpText[id*="half"] {width: calc(50% - 0.75rem); min-width: 600px;}';
-rule += '  table.cw-grid[class*="half"] {width: calc(50% - 0.75rem); min-width: 600px;}';
+
+
+rule += '  ul.cw-accordion-header[class*="half"] ' + halfSize;
+rule += "  ul.cw-accordion-header " + fullSize;
+rule += "  div.layoutSearchDocumentCustom " + fullSize;
+rule += "  div.layoutSearch " + fullSize;
+rule += '  div.CwPropertiesLayoutHelpText[id*="half"] ' + halfSize;
+rule += '  table.cw-grid[class*="half"] ' + halfSize;
 rule += "  div.cw-accordion-content div.cw-accordion-header {width: calc(100% - 16px);}";
 rule += "  div.cwPropertiesTableContainer {width: calc(50% - 3.25rem);}";
 rule += "  .cw-dashboard-container {width: calc(50% - 3.25rem); min-width: 600px;}";
-rule += "  .cw-list-wrapper {width: calc(50% - 0.5rem); min-width: 600px;}";
+rule += "  .cw-list-wrapper " + halfSize;
 
 // Load the rules and execute after the DOM loads
 window.onload = function () {
