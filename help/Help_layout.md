10ac7d246e4e15a9ae5b27c9555f53f63d9ff534
nevakee716 <nevakee716@gmail.com>
2021-02-07 17:17:11 +0100

    fix header, table, and some margin

--- help/Help_layout.md
+++ help/Help_layout.md
@@ -4,3 +4,22 @@ This package is useful when you want to use the erwin colors for your evolve web
 ## Installation  
 [https://github.com/casewise/cpm/wiki](https://github.com/casewise/cpm/wiki)  
 
+## NextGen Theme
+
+This theme is using the card system, meaning that a layout (accordeon, listbox, complexe) will be a card, and card will be display on the screen depending on their width. Layout have default size (50% or 100%), and they can be modified by adding half or full on the node ID.
+
+| Name  | Tested  |  On Home Page | Default Size  | 
+|---|---|---|---|
+| List box  | 2  | x  | half  |   
+| Accordeon  | 2  | x  | full  |   
+|  Search Layout | 2  | x  | full only | 
+|  Complexe Table | 2  |   | full only  |  
+|  HelpText | 2  | x  | full|    
+|  MemoText | 2  | x  | full|  
+|  PropertyTable | 2  | x  | half|  
+|  Flat List | 2  | x  | half|  
+|  Jvector | 2  | x  | full|  
+|  Casewise chart | 2  |   | half|
+|  Network| 2  | x  | full only|  
+|  Pivot| 2  |   | full only|  
+|  Custom Query| 1  | x  | full only but display as half|  
--- src/injectCss.js
+++ src/injectCss.js
@@ -19,13 +19,14 @@ rule += '  div.cw-accordion-content div.property-box[class*="full"] {width: 100%
 
 rule += '  ul.cw-accordion-header[class*="half"] ' + halfSize;
 rule += "  ul.cw-accordion-header " + fullSize;
+rule += "  .property-box-asso  ul.cw-accordion-header {width: 100%;}";
 rule += "  div.layoutSearchDocumentCustom " + fullSize;
 rule += "  div.layoutSearch " + fullSize;
 rule += '  div.CwPropertiesLayoutHelpText[id*="half"] ' + halfSize;
 rule += '  table.cw-grid[class*="half"] ' + halfSize;
 rule += "  div.cw-accordion-content div.cw-accordion-header {width: calc(100% - 16px);}";
-rule += "  div.cwPropertiesTableContainer {width: calc(50% - 3.25rem);}";
-rule += "  .cw-dashboard-container {width: calc(50% - 3.25rem); min-width: 600px;}";
+rule += "  div.cwPropertiesTableContainer {width: calc(50% - 3.5rem);}";
+rule += "  .cw-dashboard-container {width: calc(50% - 3.5rem); min-width: 600px;}";
 rule += "  .cw-list-wrapper " + halfSize;
 rule += " .cw-jvectormap-from-list .cwLayoutList  {width: calc(15% - 2.5rem);";
 rule += " div.empty.k-grid div.property-box.property-box-asso { width: calc(100 % - 1rem); }";
