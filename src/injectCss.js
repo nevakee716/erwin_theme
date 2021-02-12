4ab4a45b91c9af4c8ee941cb656297e93a1f9a26
nevakee716 <nevakee716@gmail.com>
2021-02-12 11:59:44 +0100

    better margin model

--- src/injectCss.js
+++ src/injectCss.js
@@ -8,38 +8,21 @@ function addCss(rule) {
 }
 
 // CSS rules
-let halfSize = "{ width : calc(50% - 1.5rem);}";
+let halfSize = "{ width : 30rem; flex: 1 0 auto; margin : 0.75rem}";
 let fullSize = "{ width : calc(100% - 0.75rem); min-width: 600px;}";
 //list box title
 let rule = "div.cw-property-details-right { width : calc(50% - 1.5rem);}";
 rule += " div.filteredListBox div.cw-property-details-right { width : calc(50% - 1.5rem);}";
-rule += "  div.property-box {width: calc(50% - 0.75rem);}";
-rule += '  div.property-box[class*="full"] {width: 100%;}';
 
-rule += '  ul.cw-accordion-header[class*="half"] ' + halfSize;
-rule += "  ul.cw-accordion-header " + fullSize;
-rule += "  .property-box-asso  ul.cw-accordion-header {width: 100%;}";
-rule += "  div.layoutSearchDocumentCustom " + fullSize;
-rule += "  div.layoutSearch " + fullSize;
-
-rule += " div.cwLayoutGraph2D " + fullSize;
-rule += '  div.cwLayoutGraph2D[id*="half"] ' + halfSize;
-rule += '  div.CwPropertiesLayoutHelpText[id*="half"] ' + halfSize;
-rule += '  table.cw-grid[class*="half"] ' + halfSize;
 rule += "  div.cw-accordion-content div.cw-accordion-header {width: calc(100% - 16px);}";
-rule += "  div.cwPropertiesTableContainer {width: calc(50% - 0.75rem - 2.5rem);}";
 rule += " .cwLayoutTimelineExpertModeConfig div.cwPropertiesTableContainer {width: calc(100% - 2.5rem);}";
-rule += "  .cw-dashboard-container {width: calc(50% - 3.5rem); min-width: 600px;}";
-rule += "  .cw-list-wrapper " + halfSize;
 
-rule += " .cw-jvectormap-from-list .cwLayoutList {width: calc(15% - 2.5rem);}";
-rule += " .cw-jvectormap-from-list > .cwLayoutList { height: calc(100% - 2.5rem);}";
-rule += " .cw-jvectormap-from-list .cwLayoutList .cwLayoutList{  width: calc(100% - 2.5rem);}";
+rule += " .cw-jvectormap-from-list .cwLayoutList {width: calc(15%);}";
+rule += " .cw-jvectormap-from-list > .cwLayoutList { height: calc(100%);}";
+rule += " .cw-jvectormap-from-list .cwLayoutList .cwLayoutList{  width: calc(100% - 0.75rem);}";
 rule += " div.empty.k-grid div.property-box.property-box-asso { width: calc(100 % - 1rem); }";
 rule += " div.empty.k-grid div.cw-list-wrapper { width: calc(100 % - 1rem); }";
-
-rule += " .cw-gantt-roadmap-lifecycle { width: 100%; }";
-rule += " .cwRadarWrapper " + halfSize;
+rule += " div.empty.k-grid div.property-box.property-box-asso {width: calc(100% - 1rem);}";
 
 // Load the rules and execute after the DOM loads
 window.onload = function () {
